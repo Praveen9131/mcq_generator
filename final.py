33d6260499a49a382a49a064559211bb238dd24d
Jogi Praveen <121826658+Praveen9131@users.noreply.github.com>
2024-07-01 23:59:16 +0530

    Update final.py

--- final.py
+++ final.py
@@ -1,8 +1,11 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 import json
+import asyncio
 from m import generate_quiz
 from c import generate_quizc
 from fill import generate_quiz1
+from match import generate
+from images import generate_content_internal  # Import the function from images.py
 
 app = Flask(__name__)
 
@@ -14,12 +17,15 @@ def generate_quiz_route():
     quiz_type = request.args.get('type', default=100, type=int)
 
     if quiz_type == 100:
-        response = generate_quiz(number, subject, tone)
+        response = generate_quiz(number, subject, tone)  # simple mcq type
     elif quiz_type == 200:
-        response = generate_quizc(number, subject, tone)
+        response = generate_quizc(number, subject, tone)  # simple checkbox type
     elif quiz_type == 300:
-        response = generate_quiz1(number,subject,tone)
-    
+        response = generate_quiz1(number, subject, tone)  # fill in the blanks
+    elif quiz_type == 400:
+        response = generate(number, subject, tone)  # Match the sequence
+    elif quiz_type == 500:
+        response = asyncio.run(generate_content_internal(subject, number))  # Calls the function to handle content generation
     else:
         response = {"error": "Invalid quiz type"}
 
